9cb7af6da4da6a54872a293bcf6c4431627f049c
Matt Kuras <mattkuras@gmail.com>
2020-03-13 18:51:51 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class Artist 
+  
+  attr_accessor :name, :songs 
+  @@all = []
+  @@songs = []
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+   def add_song(song)
+     song.artist = self 
+    #  @@songs << song
+ #    binding.pry 
+   end 
+    
+   def songs 
+    Song.all.select {|song| song.artist == self}
+   end
+   
+   def self.find_or_create_by_name(string)
+      Artist.all.each do |artist|
+        binding.pry
+       if artist.name == string
+         return artist 
+       else
+         self.new(string)
+     end
+   end
+  end 
+     
+
+    
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+require 'pry'
+class Song 
+  
+  attr_accessor :name, :artist 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.new_by_filename(filename)
+   # binding.pry 
+    song_info = filename.chomp(".mp3").split(" - ")
+   # binding.pry 
+    song = self.new(song_info[1])
+    binding.pry 
+    Artist.new(song_info[0])
+    song.artist.name = song_info[0]
+    
+  end
+  
+end

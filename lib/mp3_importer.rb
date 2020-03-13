63c9add01985b5d09cf2d9af4f7ecb996385183c
Matt Kuras <mattkuras@gmail.com>
2020-03-13 19:30:11 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,4 +4,18 @@ class MP3Importer
     @path = path 
   end
   
+   def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+  
 end
